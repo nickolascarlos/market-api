5fa6f2e91861703c500f99ec7876c39ea69f71c2
Nickolas Carlos <nickolascarlos@gmail.com>
2022-05-10 06:27:20 -0300

    refactor: Extrai as traduções customizadas para um arquivo dedicado

---
+++ src/translator/translations/custom-pt-br.ts
@@ -0,0 +1,107 @@
+export default [
+  {
+    pattern: 'seconds',
+    translation: 'segundos',
+  },
+
+  {
+    pattern: 'User not found',
+    translation: 'Usuário não encontrado',
+  },
+
+  {
+    pattern: 'Provided token is not valid or was already used',
+    translation: 'O token informado não é válido ou já foi utilizado',
+  },
+
+  {
+    pattern: 'Provided token is expired',
+    translation: 'O token informado expirou',
+  },
+
+  {
+    pattern: 'A user must be specified',
+    translation: 'Um usuário deve ser especificado',
+  },
+
+  {
+    pattern: 'Provided email is not valid',
+    translation: 'O email informado não é válido',
+  },
+
+  {
+    pattern: 'No service group with such id',
+    translation: 'Não existe grupo de serviço com o id informado',
+  },
+
+  {
+    pattern: 'No service category with such id',
+    translation: 'Não existe categoria de serviço com o id informado',
+  },
+
+  {
+    pattern: 'No service with such id',
+    translation: 'Não existe serviço com o id informado',
+  },
+
+  {
+    pattern: 'This service does not belong to the logged-in user',
+    translation: 'Esse serviço não pertence ao usuário logado',
+  },
+
+  {
+    pattern: 'No provider with such id',
+    translation: 'Não existe provedor com o id informado',
+  },
+
+  {
+    pattern: 'Provider does not belong to logged-in user',
+    translation: 'O provedor não pertence ao usuário logado',
+  },
+
+  {
+    pattern: 'Invalid credentials',
+    translation: 'Credenciais inválidas',
+  },
+
+  {
+    pattern: 'Such apiName is already in use',
+    translation: 'Esse apiName já está sendo utilizado',
+  },
+
+  {
+    pattern: 'Email is already in use',
+    translation: 'Este email já está sendo utilizado',
+  },
+
+  {
+    pattern: 'This field should not exist',
+    translation: 'Este campo não deveria existir',
+  },
+
+  {
+    pattern: 'No such service category with id {#}',
+    translation: 'Não existe categoria de serviço com o id {0}',
+  },
+
+  {
+    pattern: 'No such service group with id {#}',
+    translation: 'Não existe grupo de serviço com o id {0}',
+  },
+
+  {
+    pattern: '{#} must be a valid ISO date string: yyyy-mm-dd',
+    translation: '{0} deve estar no seguinte formato:  aaaa-mm-dd',
+  },
+
+  {
+    pattern: 'As date is provided, a timezone is needed',
+    translation:
+      'Como uma data foi informada, uma timezone também deve ser informada',
+  },
+
+  {
+    pattern: 'The category cannot be updated',
+    translation: 'A categoria não pode ser atualizada',
+  },
+];
